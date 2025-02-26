4e38d4885bedcc84bdaeec1ea2d6b6e1b11a9aa7
Matheus Henrique <matheushlm2@gmail.com>
2025-02-25 21:28:48 -0300

    Ajustando o nome da model games

--- app/Models/Games.php
+++ app/Models/Games.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Games extends Model
 {
-    protected $table = "Games";
+    protected $table = "games";
     protected $fillable = [
         "name",
         "nota",
